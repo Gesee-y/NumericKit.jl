80fc583a45a406cc3ffab54498d43a64d35fa2ae
Gesee-y <gesee37@gmail.com>
2025-03-12 18:03:20 +0100

    Update runtest.jl

--- test/runtest.jl
+++ test/runtest.jl
@@ -1,1 +1,18 @@
-﻿
+using NumericKit
+
+function main()
+    ex = :(2x^5 - 3x + 2)
+    println(typeof(ex))
+    println(ex.head)
+    println(get_children(ex))
+
+    @time der = derivative(ex)
+    @time der2 = derivative(der)
+    println("first derivative : ", der)
+    println("second derivative : ", der2)
+
+    println(eval_func(der,0))
+    println("first derivative : ", der)
+end
+
+main()
